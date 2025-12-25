6932a6fffd19edee944c5be2d806a66a925c0a15
Lemke2 <lazarlemic2@gmail.com>
2025-12-25 13:26:25 +0100

    setup project

---
+++ README.md
@@ -0,0 +1,3 @@
+Game with no dependencies only using raylib to init window and draw calls.
+
+Will replace raylib with something like SDL later.
---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Compiler and paths
+CC=clang
+RAYLIB_INCLUDE="/usr/local/include/raylib"  # Path to raylib.h
+RAYLIB_LIB="/usr/local/include/raylib/src"  # Path to libraylib.a
+OUTPUT="game.exe"
+
+# Compile command for static linking
+$CC src/main.c -I$RAYLIB_INCLUDE -L$RAYLIB_LIB -l:libraylib.a \
+    -lGL -lm -lpthread -ldl -lX11 -o $OUTPUT
+
+if [ $? -eq 0 ]; then
+    ./$OUTPUT
+fi
